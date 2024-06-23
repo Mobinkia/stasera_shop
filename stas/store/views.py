d7c3c8878637bc9f4ecde3fee3eefa6a4553d400
Mobinkia <72199902+Mobinkia@users.noreply.github.com>
2024-06-24 00:11:13 +0330

    Update views.py
    store

--- stas/store/views.py
+++ stas/store/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import *
+
+
+
+
+
+
+
+def store(request):
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created=Order.objects.get_or_create(customer=customer,compelete=False)
+        items=order.orderitem_set.all()
+        cartItems=order.get_cart_items
+    else:
+        items=[]
+        order={'get_cart_total':0,'get_cart_items':0}
+        cartItems=order['get_cart_items']
+    products=Product.objects.all()
+    categories=Category.objects.all()
+    slides=Slide.objects.all()
+    scrool=Scrool.objects.all()
+    context={'products':products,'cartItems':cartItems,'categories':categories,'slides':slides,'scrool':scrool}
+    return render(request,'store/store.html',context)
+@login_required(login_url='login')
+
 
 # Create your views here.
