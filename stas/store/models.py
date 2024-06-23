2560dd94afdf88d64ea649e13894de879d0ce7a0
alirezaaaara <alireza.abolghasemi.ara@gmail.com>
2024-06-23 18:36:20 +0330

    Update models.py
    models almost completed

--- stas/store/models.py
+++ stas/store/models.py
@@ -25,5 +25,31 @@ class Product(models.Model):
         except:
             url=""
             return url
+class Order(models.Model):
+    customer= models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    date_orderd=models.DateTimeField(auto_now_add=True)
+    compelete=models.BooleanField(default=False,null=True,blank=False)
+    def __str__(self):
+        return f'{str(self.customer)}({str(self.id)})({self.date_orderd})'
+    def compeleted(self):
+        self.compelete=True
+    @property
+    def get_cart_total(self):
+        return sum(item.get_total for item in  self.orderitem_set.all())
+    @property
+    def get_cart_items(self):
+        return sum(item.quantity for item in  self.orderitem_set.all())
+class OrderItem(models.Model):
+    product= models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
+    order= models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
+    quantity=models.IntegerField(default=0,blank=True,null=True)
+    date_aded=models.DateTimeField(auto_now_add=True)
+    @property
+    def get_total(self):
+        return self.product.price*self.quantity        
+
+# Create your models here.
+
+
 
 # Create your models here.
