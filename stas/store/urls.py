84b7a47de05773857aee802770f024bba7916844
shkasir7 <shkasiriha@gmail.com>
2024-06-24 11:38:45 +0330

    Update urls.py
    login

--- stas/store/urls.py
+++ stas/store/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', views.store,name='store'),
     path('cart/', views.cart,name='cart'),
     path('checkout/', views.checkout,name='checkout'),
+    path('login/', views.login_page,name='login'),
     
 ]
